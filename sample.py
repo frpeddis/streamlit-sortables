27a7acc79d1402ce92f1c09e865f44b1f9568c98
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 15:00:33 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -9,30 +9,30 @@ st.write('Sort items in a single container.')
 items = ['item1', 'item2', 'item3']
 sorted_items = sort_items(items)
 st.write(sorted_items)
+#
 
+#st.write('----')
+#st.write('Sort items in multiple containers.')
+#items = [
+#    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
+#    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
+#]
+#sorted_items = sort_items(items, multi_containers=True)
+#st.write(sorted_items)
 
-st.write('----')
-st.write('Sort items in multiple containers.')
-items = [
-    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-]
-sorted_items = sort_items(items, multi_containers=True)
-st.write(sorted_items)
-
-st.write('----')
-st.write('Lots of items in a single container.')
-items = [
-    {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
-]
-sorted_items = sort_items(items, multi_containers=True)
-st.write(sorted_items)
+#st.write('----')
+#st.write('Lots of items in a single container.')
+#items = [
+#    {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
+#]
+#sorted_items = sort_items(items, multi_containers=True)
+#st.write(sorted_items)
 
 st.write('----')
 st.write('Sort items in multiple containers with vertical direction.')
 items = [
-    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
+    {'header': 'Ordinali', 'items': ['item1', 'item2', 'item3']},
+    {'header': 'Random', 'items': ['item1', 'item2', 'item3','item4', 'item5', 'item6']},
 ]
 sorted_items = sort_items(items, multi_containers=True, direction="vertical")
 st.write(sorted_items)
