570a02757e70e254d25fa89a7b8785342990072a
frpeddis <97230694+frpeddis@users.noreply.github.com>
2023-12-26 17:58:53 +0100

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,38 +1,38 @@
 import streamlit as st
+import pandas as pd
 from streamlit_sortables import sort_items
 
-import streamlit as st
+# Titolo dell'applicazione
+st.title('Scoperte Invenzioni')
 
-st.title('Sortables')
+# Carica il file CSV
+uploaded_file = st.file_uploader("Carica un file CSV", type="csv")
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+else:
+    st.info("Attendi il caricamento del file CSV.")
+    data = pd.DataFrame()
 
-st.write('Sort items in a single container.')
-items = ['item1', 'item2', 'item3']
-sorted_items = sort_items(items)
-st.write(sorted_items)
+# Se i dati sono sufficienti, seleziona 5 record casuali
+if not data.empty and len(data) >= 5:
+    if 'selected_records' not in st.session_state:
+        st.session_state['selected_records'] = data.sample(5)
 
+    # Mostra le invenzioni casuali
+    st.write('Invenzioni Casuali:')
+    items = [{'header': 'Invenzioni', 'items': list(st.session_state['selected_records']['Descrizione Breve'])}]
 
-st.write('----')
-st.write('Sort items in multiple containers.')
-items = [
-    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-]
-sorted_items = sort_items(items, multi_containers=True)
-st.write(sorted_items)
+    # Utilizza streamlit-sortables per ordinare gli elementi
+    sorted_items = sort_items(items, multi_containers=True, direction="vertical")
 
-st.write('----')
-st.write('Lots of items in a single container.')
-items = [
-    {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
-]
-sorted_items = sort_items(items, multi_containers=True)
-st.write(sorted_items)
+    # Verifica l'ordine
+    if st.button("Verifica Ordine"):
+        ordered_records = pd.DataFrame()
+        for desc in sorted_items[0]['items']:
+            ordered_records = ordered_records.append(st.session_state['selected_records'][st.session_state['selected_records']['Descrizione Breve'] == desc])
 
-st.write('----')
-st.write('Sort items in multiple containers with vertical direction.')
-items = [
-    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-]
-sorted_items = sort_items(items, multi_containers=True, direction="vertical")
-st.write(sorted_items)
+        ordered_correctly = ordered_records['anno della scoperta'].is_monotonic_increasing
+        if ordered_correctly and len(ordered_records) == len(sorted_items[0]['items']):
+            st.success("Hai indovinato l'ordine corretto!")
+        else:
+            st.error("Ordine non corretto. Riprova.")
